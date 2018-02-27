ea6ebf39c6563ca87fdf2a7369f41c27614331bb
SondosAzz <sondos.azzouz@gmail.com>
2018-02-27 21:32:00 +0300

    collision detection

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var score=0;
+var score = 0;
 // Enemies our player must avoid
 var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
@@ -8,6 +8,16 @@ var Enemy = function(x,y) {
     this.speed = getRandomInt(50, 300);
     this.sprite = 'images/enemy-bug.png';
 };
+
+function gameOver(){
+  console.log("collision");
+  oldScore = score
+  alert("GAMEOVER, your score is "+ oldScore);
+  score = 0;
+  document.getElementById("score").innerHTML  = "Score: 0";
+  player.reset();
+
+}
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -19,17 +29,18 @@ Enemy.prototype.update = function(dt) {
     this.x = -50;
     }
 
-//Enemy-player collision :
-    allEnemies.forEach(function(enemy,i){
-      if( (player.y-10)==(this.y)&&(player.x > this.x-50)&&(player.x < this.x+50) )
-      {  console.log("collision");
-        alert("GAMEOVER, yor current score is \n"+ score);
-        document.getElementById("score").innerHTML  = "Score: 0";
-        player.reset();
+//Enemy-player collision GAMEOVER :
+// In refrence to MDN - Mozilla found on  https://developer.mozilla.org/kab/docs/Games/Techniques/2D_collision_detection.
+  allEnemies.forEach(function(enemy){
+    if  (player.x < enemy.x + 50 &&
+ player.x + 50 > enemy.x &&
+ player.y < enemy.y + 50 &&
+ 50 + player.y > enemy.y)
+      {
+         gameOver();
       }
+
     });
-
-
 
 }
 
@@ -65,16 +76,13 @@ Player.prototype.update = function(dt) {
     this.y = 530;
   }
 
-
-
-
-
 }
 
 Player.prototype.reset = function() {
 
   this.x = 200;
   this.y = 450;
+
 
 }
 
@@ -89,16 +97,16 @@ Player.prototype.handleInput = function(key) {
 
   switch (key){
     case 'right':
-        this.x +=40;
+        this.x +=50;
         break;
     case 'left':
-      this.x -=40;
+      this.x -=50;
       break;
     case 'down':
-      this.y +=30;
+      this.y +=50;
       break;
     case 'up':
-        this.y -=40;
+        this.y -=50;
        break;
   }
 
@@ -113,10 +121,10 @@ function getRandomInt(min, max) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-//var enemy = new Enemy(60,100);
+//var enemy = new Enemy(200,200); allEnemies.push(enemy);
 var allEnemies = []
 for (i=1;i<=5;i++){
-  var yValue = getRandomInt(50, 240)
+  var yValue = getRandomInt(50, 140)
   enemy = new Enemy(-10,yValue);
   allEnemies.push(enemy);
 }
@@ -135,7 +143,6 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
+player.handleInput(allowedKeys[e.keyCode]);
 
-    player.handleInput(allowedKeys[e.keyCode]);
-    console.log(allowedKeys[e.keyCode]);
 });
